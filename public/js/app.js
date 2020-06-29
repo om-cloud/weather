64379275ef8413362879f9d69840c5bfd17fbc07
om-cloud <ohad.matalon@gmail.com>
2020-06-30 00:43:33 +0300

    andrew app.js code

--- public/js/app.js
+++ public/js/app.js
@@ -1,63 +1,86 @@
 
 
-const weatherForm = document.querySelector('form');
-const search = document.querySelector('input');
-const message1 = document.querySelector('#message-1');
-const message2 = document.querySelector('#message-2');
+// const weatherForm = document.querySelector('form');
+// const search = document.querySelector('input');
+// const message1 = document.querySelector('#message-1');
+// const message2 = document.querySelector('#message-2');
 
-// const getWeather = (location)=>{ 
+// // const getWeather = (location)=>{ 
+// //     fetch('/weather?address='+ location).then((response)=>{
+
+// //     response.json().then((data)=>{
+// //         if(data.error){
+// //             message1.textContent=data.error;
+// //             message2.textContent='';
+// //            return //console.log(data.error);  
+// //         }else{
+// //             //console.log(data.location);
+// //             //console.log(data.forecast);
+// //             message1.textContent=data.location;
+// //             message2.textContent=data.forecast;
+// //         }
+
+// //     })
+// // })
+// // }
+
+
+
+// weatherForm.addEventListener('submit', (event)=>{
+//     event.preventDefault();  // prevent refreshing of the page
+//     const location = search.value;
+//     if(!location){
+//         message1.textContent='Please Enter a Location';
+//         message2.textContent='';
+//         return //console.log('Please Enter a Location');
+//     }
+//     message1.textContent='Loading...';
+//     message2.textContent='';
+//     //getWeather(location);
 //     fetch('/weather?address='+ location).then((response)=>{
 
-//     response.json().then((data)=>{
-//         if(data.error){
-//             message1.textContent=data.error;
-//             message2.textContent='';
-//            return //console.log(data.error);  
-//         }else{
-//             //console.log(data.location);
-//             //console.log(data.forecast);
-//             message1.textContent=data.location;
-//             message2.textContent=data.forecast;
-//         }
-
-//     })
-// })
-// }
-
-
-
-weatherForm.addEventListener('submit', (event)=>{
-    event.preventDefault();  // prevent refreshing of the page
-    const location = search.value;
-    if(!location){
-        message1.textContent='Please Enter a Location';
-        message2.textContent='';
-        return //console.log('Please Enter a Location');
-    }
-    message1.textContent='Loading...';
-    message2.textContent='';
-    //getWeather(location);
-    fetch('/weather?address='+ location).then((response)=>{
-
-        response.json().then((data)=>{
-            if(data.error){
-                message1.textContent=data.error;
-                message2.textContent='';
-               return //console.log(data.error);  
-            }else{
-                //console.log(data.location);
-                //console.log(data.forecast);
-                message1.textContent=data.location;
-                message2.textContent=data.forecast;
-            }
+//         response.json().then((data)=>{
+//             if(data.error){
+//                 message1.textContent=data.error;
+//                 message2.textContent='';
+//                return //console.log(data.error);  
+//             }else{
+//                 //console.log(data.location);
+//                 //console.log(data.forecast);
+//                 message1.textContent=data.location;
+//                 message2.textContent=data.forecast;
+//             }
     
+//         })
+//     })
+//     //console.log(location);
+// })
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
         })
     })
-    //console.log(location);
 })
-
-
-
 
 
 
